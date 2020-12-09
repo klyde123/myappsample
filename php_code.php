2c3e4c407d867532b47b23ada858d260e0cc6153
Klyde Louise Olitoquit <kl.olitoquit@gmail.com>
2020-12-09 19:56:01 +0800

    adding connection to database

--- php_code.php
+++ php_code.php
@@ -7,3 +7,12 @@
 	$discription = "";
 	$id = 0;
 	$update = false;
+
+	if (isset($_POST['save'])) {
+		$name = $_POST['name'];
+		$discription = $_POST['discription'];
+
+		mysqli_query($db, "INSERT INTO info (name, discription) VALUES ('$name', '$discription')"); 
+		$_SESSION['message'] = "Description saved"; 
+		header('location: index.php');
+	}
